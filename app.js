f22cca65f9c2091bde7eb4110c33ae149b75c005
Christy Shafack <60237199+ChristyShafack@users.noreply.github.com>
2024-03-06 14:45:15 +0000

    MA portfolio preparation

---
+++ app.js
@@ -0,0 +1,43 @@
+require('dotenv').config()
+
+const app = require('./config')
+
+app.listen(app.get('PORT'), () => {})
+
+app.get('/', (request, response) => {
+  response.render('pages/home')
+})
+app.get('/projects', (request, response) => {
+  response.render('pages/projects')
+})
+app.get('/blogs', (request, response) => {
+  response.render('pages/blogs')
+})
+
+app.get('/about', (request, response) => {
+  response.render('pages/about')
+})
+
+
+// app.get('/projects/portraits', (request, response) => {
+//   response.render('projects/portraits')
+// })
+
+// app.get('/projects/darkroom', (request, response) => {
+//   response.render('projects/darkroom')
+// })
+
+
+app.use((request, response) => {
+  response.status(404)
+
+  if (request.accepts('html')) {
+    return response.redirect('/')
+  }
+
+  if (request.accepts('json')) {
+    return response.send({ error: 'Not Found' })
+  }
+
+  response.type('txt').send('Not Found')
+})
